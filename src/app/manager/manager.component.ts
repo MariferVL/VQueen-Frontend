09e3c15e2c7b51393e62a673d3836a71abbf85d7
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-23 15:26:49 -0400

    update admin login with isLoggedIn and logout

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-manager',
@@ -8,10 +9,22 @@ import { Title } from '@angular/platform-browser';
 })
 export class ManagerComponent {
   constructor(
-    private titleService: Title
+    private titleService: Title,
+    private authService: AuthService
     ) {}
 
   ngOnInit() {
     this.titleService.setTitle('VQAdmin - Main');
   }
+
+  isLoggedIn(): boolean {
+    return !!this.authService.accessToken;
+  }
+  
+  logout(): void {
+    this.authService.accessToken = undefined;
+  }
 }
+
+
+
