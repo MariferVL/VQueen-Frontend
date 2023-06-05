0359beb91f25bc0462e111d8a46b73911a237727
Marifer Villalobos <mfvl.1186@gmail.com>
2023-06-05 02:17:05 -0400

    set employees modal style

--- src/app/components/modals/employee-modal/employee-modal.component.ts
+++ src/app/components/modals/employee-modal/employee-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { User } from 'src/app/types';
 
 @Component({
   selector: 'app-employee-modal',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-modal.component.css']
 })
 export class EmployeeModalComponent {
+  employees: User[] = [];
+
+  constructor(
+    public addEmployeeRef: MatDialogRef<EmployeeModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { employees: User[] }
+  ) {
+    this.employees = data.employees;
+  }
+
+  submitForm({ id, email, password, role }:
+    { id: number, email: string, password: string, role: string }): void {
+    const data = [id, email, password, role]
+    console.log('Sendig Employee Form: ', data);
+    this.addEmployeeRef.close(data);
+  }
+
+  close(): void {
+    this.addEmployeeRef.close();
+  }
 
 }
