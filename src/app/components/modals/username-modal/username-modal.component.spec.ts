fc3dfcad421367996a0a02e9bceb50602980bc06
Marifer Villalobos <mfvl.1186@gmail.com>
2023-06-06 01:53:14 -0400

    create test to username modal component

--- src/app/components/modals/username-modal/username-modal.component.spec.ts
+++ src/app/components/modals/username-modal/username-modal.component.spec.ts
@@ -1,21 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UsernameModalComponent } from './username-modal.component';
 
 describe('UsernameModalComponent', () => {
   let component: UsernameModalComponent;
   let fixture: ComponentFixture<UsernameModalComponent>;
+  let dialogRef: MatDialogRef<UsernameModalComponent>;
+
+  beforeEach(waitForAsync(() => {
+    dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
+    TestBed.configureTestingModule({
+      declarations: [UsernameModalComponent],
+      imports: [FormsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UsernameModalComponent]
-    });
     fixture = TestBed.createComponent(UsernameModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submit the customer name', () => {
+    spyOn(console, 'log');
+    component.customerName = 'John Doe';
+    component.submitCustomerName();
+    expect(console.log).toHaveBeenCalledWith('Submitted Customer Name:', 'John Doe');
+    expect(dialogRef.close).toHaveBeenCalledWith('John Doe');
+  });
+
+  it('should close the dialog', () => {
+    component.close();
+    expect(dialogRef.close).toHaveBeenCalled();
   });
 });
